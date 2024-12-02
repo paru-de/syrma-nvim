b3a1dd18ea3d6e96012c6a38eb30ea50be31406b
paru-de <13712334+paru-de@users.noreply.github.com>
2024-12-02 17:28:38 +0100

    yazi: purge lazyspec stuff, make yazi default instead of netrw

--- lua/myLuaConf/plugins/ui/yazi.lua
+++ lua/myLuaConf/plugins/ui/yazi.lua
@@ -1,21 +1,20 @@
-require('lze').load {
+require("lze").load({
 	{
 		"yazi.nvim",
-		for_cat = 'general.extra',
+		for_cat = "general.extra",
 		-- cmd = { "" },
 		event = "DeferredUIEnter",
 		-- ft = "",
 		-- keys = "",
 		-- colorscheme = "",
 		after = function(plugin)
-			require('yazi').setup({
-				opts = {
-					open_for_directories = true,
-					keymaps = {
-						show_help = '<f1>',
-					},
+			require("yazi").setup({
+
+				open_for_directories = true,
+				keymaps = {
+					show_help = "<f1>",
 				},
 			})
 		end,
 	},
-}
+})
