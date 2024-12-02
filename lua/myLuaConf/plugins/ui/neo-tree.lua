5c0fcc06d22156f2aee8d750af81675e80b7b06c
paru-de <13712334+paru-de@users.noreply.github.com>
2024-12-02 16:55:54 +0100

    add lze for neo-tree

--- lua/myLuaConf/plugins/ui/neo-tree.lua
+++ lua/myLuaConf/plugins/ui/neo-tree.lua
@@ -1,12 +1,18 @@
-require("neo-tree").setup({
-	version = "*",
-	cmd = "Neotree",
-	event_handlers = {
-		{
-			event = "file_open_requested",
-			handler = function()
-				require("neo-tree.command").execute({ action = "close" })
-			end,
-		},
-	},
+require("lze").load({
+	for_cat = "general.extra",
+
+	after = function(plugin)
+		require("neo-tree").setup({
+			version = "*",
+			cmd = "Neotree",
+			event_handlers = {
+				{
+					event = "file_open_requested",
+					handler = function()
+						require("neo-tree.command").execute({ action = "close" })
+					end,
+				},
+			},
+		})
+	end,
 })
